bd9c6d3aa1b292caf905b4440dca6df456d82d72
Andrey Efimov <aiefimov@sberbank.ru>
2023-06-11 21:49:13 +0300

    homework activity 2

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,12 +1,28 @@
 package otus.gpb.homework.activities.receiver
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReceiverActivity : AppCompatActivity() {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        val title = intent.getStringExtra("title")
+        val year = intent.getStringExtra("year")
+        val description = intent.getStringExtra("description")
+
+        findViewById<TextView>(R.id.descriptionTextView).text = description
+        findViewById<TextView>(R.id.titleTextView).text = title
+        findViewById<TextView>(R.id.yearTextView).text = year
+        when (title.toString()) {
+            "Славные парни" -> findViewById<ImageView>(R.id.posterImageView).setImageDrawable(getDrawable(R.drawable.niceguys))
+            "Интерстеллар" -> findViewById<ImageView>(R.id.posterImageView).setImageDrawable(getDrawable(R.drawable.interstellar))
+        }
     }
 }
